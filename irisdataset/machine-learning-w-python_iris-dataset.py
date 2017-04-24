770e3d41fb242a9b0c8defd98a0abe1900b506e2
admin <dnzengou@gmail.com>
2017-04-24 11:32:17 +0200

    add docs and scripts .py

---
+++ irisdataset/machine-learning-w-python_iris-dataset.py
@@ -0,0 +1,144 @@
+## (Recommended) create a virtual environment
+## first, install virtualenv
+# pip install virtualenv
+## Create a virtual environment for a project
+# cd my_project_folder
+# virtualenv venv  --no-site-packages
+## Install all other packages into the virtual environment
+# cd venv
+# virtualenv pymote_env
+## Activate the virtual environment
+# venv\Scripts\activate
+## (venv) C:\Users\dez\Documents\Python Scripts\py_project_folder>
+## OR
+# activate
+## to deactivate the venv
+# deactivate
+
+##  To keep your environment consistent, “freeze” the current state of the environment packages
+# pip freeze > requirements.txt
+## install packages from requirements.txt
+# pip install -r requirements.txt
+
+# 1. Downloading, Installing and Starting Python SciPy
+# Check the versions of libraries
+
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+# Output
+## Python: 3.5.2 :Anaconda 4.2.0 (64-bit): (default, Jul  5 2016, 11:41:13) [MSC v.1900 64 bit (AMD64)]
+## scipy: 0.18.1
+## numpy: 1.11.1
+## matplotlib: 1.5.3
+## pandas: 0.18.1
+## sklearn: 0.17.1
+
+# 2. Load The Data
+
+# Load libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+# shape
+print(dataset.shape)
+# (150, 5)
+
+# head
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('class').size())
+
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+
+# histograms
+dataset.hist()
+plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
+
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+ 
+ # Compare Algorithms
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+# Make predictions on validation dataset
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
